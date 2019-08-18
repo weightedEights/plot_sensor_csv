27a53c58fea0dbfb6706fab30e847d09b02599e3
weightedEights <poormanjosh@gmail.com>
2019-08-18 05:33:01 -0700

    update interpreter with xarray and supporting packages

--- plot_sensor_csv.py
+++ plot_sensor_csv.py
@@ -77,7 +77,7 @@ def pandas_plot(df):
 
 def bokeh_plot(df):
 
-    filtered_df = df[(df["temp_bottom"] > -25) & (df["temp_bottom"] < 20)]
+    # filtered_df = df[(df["temp_bottom"] > -25) & (df["temp_bottom"] < 20)]
 
     output_file("interior_temps.html")
 
@@ -96,8 +96,8 @@ def bokeh_plot(df):
 
     p = figure(x_axis_type="datetime", plot_width=1600, plot_height=900, tools=tools)
 
-    p.line(x=filtered_df.index, y=filtered_df.iloc[:, 0], legend="Top Shelf Temp", line_width=2, color="red")
-    p.line(x=filtered_df.index, y=filtered_df.iloc[:, 1], legend="Bottom Shelf Temp", line_width=2, color="blue")
+    p.line(x=df.index, y=df.iloc[:, 0], legend="Top Shelf Temp", line_width=2, color="red")
+    p.line(x=df.index, y=df.iloc[:, 1], legend="Bottom Shelf Temp", line_width=2, color="blue")
 
     p.title.text = "Interior Temps, Orange Fish Hut #1"
     p.yaxis.axis_label = "Temp [\N{DEGREE SIGN}C]"
